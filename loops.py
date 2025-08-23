4250e07455ddde251694f667de5dbb0fc7b35b6f
arashsaadatmand <153981016+arashsaadatmand@users.noreply.github.com>
2025-08-23 16:24:19 +0330

    Add files via upload

---
+++ loops.py
@@ -0,0 +1,11 @@
+fruits = ["apple", "banana", "cherry"]
+for f in fruits:
+    print("Fruit:", f)
+
+for i in range(5):
+    print("Number:", i)
+
+count = 0
+while count < 3:
+    print("Count:", count)
+    count = count + 1
