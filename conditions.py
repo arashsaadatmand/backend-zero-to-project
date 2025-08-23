ae1555931d07d09e4c718981ba0e231bcb88c38c
arashsaadatmand <153981016+arashsaadatmand@users.noreply.github.com>
2025-08-23 15:04:07 +0330

    Add files via upload

---
+++ conditions.py
@@ -0,0 +1,12 @@
+score = int(input("Enter your score: "))
+
+if score >= 90:
+    print("Grade: A")
+elif score >= 70:
+    print("Grade: B")
+elif score >= 50:
+    print("Grade: C")
+else:
+    print("Grade: F")
+
+print("Done!")
